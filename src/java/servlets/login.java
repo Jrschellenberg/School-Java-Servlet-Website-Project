12e0df48ea129acc10db9ff73170d64817a2e4ce
Meezo <>
2016-02-08 02:15:28 -0600

    Register form, merge logout, fix index menu, change login bean name *basic Register form whith checking if username already exists *merging the logout file with the controller *index menu is dynamic now *only one bean (UserValues) for (login & Register)

--- src/java/servlets/login.java
+++ src/java/servlets/login.java
@@ -14,14 +14,24 @@ public class login {
         ResultSet resultSet = DBconnection.db.query("SELECT * FROM users Where user_username='" + username + "' ");
 
         while (resultSet.next()) {
-
             //Look for password
-            if (resultSet.getString("user_password").equalsIgnoreCase(password)) {
+            if (resultSet.getString("user_password").equals(password)) {
                 return true;
             }
-
         }
 
+        return false;
+    }
+    
+    public Boolean userTypeAdmin(String username) throws ClassNotFoundException, SQLException {
+        
+        DBconnection.db.getDbCon();
+        ResultSet resultSet = DBconnection.db.query("SELECT * FROM users WHERE user_username='" + username + "' AND User_type =0 ");
+        
+        while(resultSet.next()){
+                return true;           
+        }
+        
         return false;
     }
 }
